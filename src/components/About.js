9913e9ddb6f71765c0dfbc3884fec2fb46ae09a1
Laurie Roy <laurieroy.dev@gmail.com>
2023-01-02 08:51:32 -0800

    Add About section

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import styles from "./About.module.css";
+
+import { about } from "../content/about";
+
+export const About = () => {
+  return (
+    <section className={styles.wrapper}>
+      {about.map((item, i) => {
+        return (
+          <div classname={styles.card} key={i}>
+            <div className={styles.info}>
+              <h1 className={styles.title}>{item.title}</h1>
+              <p className={styles.description}>{item.duration}</p>
+            </div>
+            <div className={styles.info}>
+              <img className={styles.icon} src={item.icon} alt="icon" />
+            </div>
+          </div>
+        );
+      })}
+    </section>
+  );
+};
