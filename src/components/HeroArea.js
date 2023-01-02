5a8639b4c4d1bbf8dd9fb6e315b144c4f1a7ebb1
Laurie Roy <laurieroy.dev@gmail.com>
2023-01-02 08:37:04 -0800

    Add the HeroArea section

---
+++ src/components/HeroArea.js
@@ -0,0 +1,31 @@
+import styles from "./HeroArea.module.css";
+
+export const HeroArea = () => {
+  return (
+    <section className={styles.wrapper}>
+      <img src="/assets/profile.png" alt="profile" className={styles.profile} />
+      <h1 className="title">Hi, I'm Laurie!</h1>
+      <p className="description">An anspiring web developer looking for a new role i tech</p>
+      <div className={styles.buttonBox}>
+        <button
+          className={`${styles.button} ${styles.project}`}
+          onClick={()=> {
+            document.getElementById("projects")
+              .scrollIntoView({behavior: "smooth"});
+          }}
+        >
+          My Projects
+        </button>
+        <button
+          className={`${styles.button} ${styles.hire}`}
+          onClick={()=>{
+            document.getElementById("contact")
+              .scrollIntoView({behavior: "smooth"});
+          }}
+        >
+          Hire me
+        </button>
+      </div>
+    </section> 
+  )
+}
